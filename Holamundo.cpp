7d578b8e28e7d20911f5ed6f80c78e316c57f2c3
Juan Restrepo <jplulo1996@gmail.com>
2023-01-30 11:23:20 -0500

    Punto main

--- Holamundo.cpp
+++ Holamundo.cpp
@@ -2,12 +2,5 @@
 using namespace std;
 
 int main() {
-    int edad = 10;
-    const char letra = 'A';
-    int lista_edades [] = {18, 24, 16};
-    lista_edades[1] = 26;
-    cout << "Hola mundo\n";
-    cout << edad << endl;
-    cout << letra << endl;
-    cout << lista_edades[1];
+    
 }
