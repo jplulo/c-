43870e775199b3b89651fca6be15e348f059cf0c
Juan Restrepo <jplulo1996@gmail.com>
2023-01-30 18:59:07 -0500

    String

--- Holamundo.cpp
+++ Holamundo.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    
+    string texto = "Hola mundo";    
+    string numero = "10";
+    cout << texto << endl;
+    cout << texto.size();
+    cout << stoi(numero);
 }
