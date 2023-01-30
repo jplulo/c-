dcf9ce6243266788bc95cf3ae4eca3936ab823ae
Juan Restrepo <jplulo1996@gmail.com>
2023-01-30 18:13:41 -0500

    Funciones

--- Holamundo.cpp
+++ Holamundo.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
+int suma(int a, int b){
+    return a + b;
+}
+
+int multi(int a, int b){
+    return a * b;
+}
+
 int main() {
-    
+    cout << suma(2, 2) << endl;
+    cout << multi(10, 2);
+
 }
