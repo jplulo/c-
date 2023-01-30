a0efff9d71c55725840e5f650faea92bbaa625d1
Juan Restrepo <jplulo1996@gmail.com>
2023-01-30 12:44:13 -0500

    Condicional if

--- Holamundo.cpp
+++ Holamundo.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main() {
+    int edad = 0;
+    cout << "Ingresa tu edad: ";
+    cin >> edad;
+    if (edad >= 18 && edad <= 40)
+    {
+        cout << "Puedes votar";
+    }else{
+        cout << "No puedes votar";
+    }
     
 }
