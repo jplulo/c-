dd4b55644a68f53dc3e4a1b4f8c7451b1457e4aa
Juan Restrepo <jplulo1996@gmail.com>
2023-01-30 13:26:31 -0500

    Switch case

--- Holamundo.cpp
+++ Holamundo.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
-    
+    int option = 0 ;
+    cout << "Introduce una opcion: ";
+    cin >> option;
+    switch (option)
+    {
+    case 1:
+        cout << "Opcion 1";
+        break;
+    case 2:
+        cout << "Opcion 2";
+        break;
+    default:
+        cout << "Nothing";
+        break;
+    }    
 }
