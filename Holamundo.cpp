3a4cc91d124ea17848f5ebc1f6a172e2c582e7fe
Juan Restrepo <jplulo1996@gmail.com>
2023-01-30 14:01:37 -0500

    Trabajo independiente de numeros pares

--- Holamundo.cpp
+++ Holamundo.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
+    int limite = 0;
+    cout << "Introduce el limite: ";
+    cin >> limite;
+    for (int i = 1; i <= limite; i++)
+    {
+        if (i % 2 == 0)
+        {
+            cout << i << endl;
+        }
+        
+    }
     
 }
